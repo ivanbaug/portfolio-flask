5d3e75bb9a940c2308a8ae6453ac0dabc7f8e79e
ivanbaug <ivsitioinfo@gmail.com>
2021-09-24 22:17:51 -0500

    initial commit

---
+++ project/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("APP_KEY")
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
