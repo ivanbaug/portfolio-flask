a2a5b5f894bba1482819b261514b364091482c51
ivanbaug <ivsitioinfo@gmail.com>
2022-01-25 11:10:27 -0500

    improve page selector usage and style

--- project/static/js/display-prjs.js
+++ project/static/js/display-prjs.js
@@ -4,11 +4,38 @@ const firstItem = document.getElementById('first-pg-item')
 const ulPagination = document.getElementById('list-pagination')
 
 // pagesElement.getElementsByClassName(
-const cardNumber = 5
+const cardNumber = 3
 
 const pageChangeHandler = (data, pageNum) => {
   const page = data.getAttribute("data-option")
   displayPrjPage(page)
+
+  firstItem.querySelector('.page-link').setAttribute('data-option', `${Number(page) - 1}`)
+  lastItem.querySelector('.page-link').setAttribute('data-option', `${Number(page) + 1}`)
+  // Enabe/Disable previous and next buttons when they dont apply anymore
+  if (page > 1) {
+    firstItem.classList.remove('disabled')
+  }
+  else {
+    firstItem.classList.add('disabled')
+    firstItem.querySelector('.page-link').setAttribute('data-option', '-1')
+  }
+
+  if (Number(page) === Number(pageNum)) {
+    lastItem.classList.add('disabled')
+    lastItem.querySelector('.page-link').setAttribute('data-option', '-1')
+  }
+  else {
+    lastItem.classList.remove('disabled')
+  }
+
+  // Clear active states
+  Array.from(ulPagination.getElementsByClassName('page-item')).forEach(li => li.classList.remove('active'))
+
+  // Set current page as active
+  const currentPg = ulPagination.getElementsByClassName('page-item')[page]
+  currentPg.classList.add('active')
+
 
 }
 
@@ -39,26 +66,23 @@ if (projects) {
     // Create page items
     for (let i = 1; i <= pageNum; i++) {
       let pgItem = document.createElement('li')
-
       pgItem.classList.add('page-item')
-      // pgItem.classList.remove('page-item')
-
+      i === 1 && pgItem.classList.add('active')
       pgItem.innerHTML = `
       <button 
         class="page-link" 
         data-option="${i}"
-        onclick="pageChangeHandler(this, pageNum)"
-        >${i}</button>
-      `
+        onclick="pageChangeHandler(this, ${pageNum})" >
+          ${i}
+      </button> `
       ulPagination.insertBefore(pgItem, lastItem)
-      // console.log('appended')
     }
-
+    lastItem.querySelector('.page-link').setAttribute('onclick', `pageChangeHandler(this, ${pageNum})`)
+    firstItem.querySelector('.page-link').setAttribute('onclick', `pageChangeHandler(this, ${pageNum})`)
   }
   else {
     const pagesElement = document.getElementById('projects-pagination')
     pagesElement.style.display = 'none'
-    // pagesElement.style.display = 'block'
   }
 
 }
